36fccf446fe433c843b043b484ea4da4b320222a
Sanchittanwar30 <111452909+Sanchittanwar30@users.noreply.github.com>
2024-10-22 05:35:54 +0530

    u8

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </p>
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img className="w-full max-w-[360px]" src={assets.about_image} alt="" />
+
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 tezt-sm text-gray-600 ">
+          <p>
+            Welcome to Prescripto , Your Trusted Partner in managing yout
+            healthcare needs conveniently and effeciently. At Prescripto we
+            understand the challenge individuals face when it comes to
+            scheduling doctor appointments and managing their health records
+          </p>
+          <p>
+            Prescripto is commited to excellence in healthcare technology. We
+            continously strive to enhance our platform, Integrating the latest
+            advancement to improve user experience and deliver best service.
+            Whether you're booking your first appointment or managing ongoing
+            care , We are here to support you every step on the way{" "}
+          </p>
+          <b className="text-gray-800">Our Vision </b>
+          <p>
+            Our Vision at Prescripto is to create a seamless healthcare
+            experience for every user. We aim to bridge the gap between patients
+            and healtcare providers , making it easier for You to access the
+            care You need , when You need it{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className="text-xl my-4">
+        <p>
+          WHY <span className="text-gray-700 font-semibold">CHOOSE US</span>
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer ">
+          <b>EFFICIENCY:</b>
+          <p>
+            Streamlined appoitnment scheduling that fits into your busy
+            lifestyle.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>CONVENIENCE:</b>
+          <p>
+            Access to a network of trusted healthcare professionals in your
+            area.{" "}
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>PERSONALIZATION:</b>
+          <p>
+            Tailored Recommendations and reminders to help you stay on top of
+            your health
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          CONTACT <span className="text-gray-700 font-semobold">US</span>
+        </p>
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.contact_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semobold text-lg text-gray-600">OUR OFFICE</p>
+          <p className="text-gray-500">
+            100 baker street <br /> milton road , Raftel , OP
+          </p>
+          <p className="text-gray-500">
+            Tel : +91 99999-xxxxx <br /> sanchez.xxx@gmail.com
+          </p>
+          <p className="text-gray-500">Careers at Prescripto</p>
+          <p className="text-gray-500">Learn more about Us</p>
+
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            {" "}
+            Explore{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
